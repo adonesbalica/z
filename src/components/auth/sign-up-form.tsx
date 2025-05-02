643a2bfd1b222e6df243a5b67452f34a5f1330b9
Adones Balica <adonesbalica@gmail.com>
2025-05-02 17:49:51 -0300

    feat: sign up page

---
+++ src/components/auth/sign-up-form.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Input } from "../ui/Input";
+import { Button } from "../ui/Button";
+
+export const SignUpForm = () => {
+  const router = useRouter();
+
+  const [nameField, setNameField] = useState("");
+  const [emailFiled, setEmailField] = useState("");
+  const [passwordFiled, setPasswordField] = useState("");
+
+  const handleEnterButton = () => {
+    router.replace("/home");
+  };
+
+  return (
+    <>
+      <Input
+        placeholder="Digite seu nome"
+        value={nameField}
+        onChange={setNameField}
+      />
+
+      <Input
+        placeholder="Digite seu e-mail"
+        value={emailFiled}
+        onChange={setEmailField}
+      />
+
+      <Input
+        placeholder="Digite sua senha"
+        value={passwordFiled}
+        onChange={setPasswordField}
+        password
+      />
+
+      <Button label="Ciar conta" size={1} onClick={handleEnterButton} />
+    </>
+  );
+};
