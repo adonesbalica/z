3f6423ff38254d1903d1463ed2d07478bf5a91f0
Adones Balica <adonesbalica@gmail.com>
2025-05-02 19:22:07 -0300

    feat: navlink component

---
+++ src/components/nav/nav-item.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Props = {
+  label: string;
+  icon: IconDefinition;
+  href: string;
+  active?: boolean;
+};
+
+export const NavItem = ({ href, icon, label, active }: Props) => {
+  const pathname = usePathname();
+  const isMe = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center gap-6 py-3 ${
+        active || isMe ? "opacity-100" : "opacity-70"
+      } hover:opacity-100`}
+    >
+      <FontAwesomeIcon icon={icon} className="size-6" />
+      {label}
+    </Link>
+  );
+};
