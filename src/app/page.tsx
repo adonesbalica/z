82445e9b805f1f51f8cbaa5f81c60dbe9a7f7b61
Adones Balica <adonesbalica@gmail.com>
2025-05-02 18:07:32 -0300

    feat: home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <div>Some page</div>;
+  redirect("/home");
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Logo size={80} />
+    </div>
+  );
 }
